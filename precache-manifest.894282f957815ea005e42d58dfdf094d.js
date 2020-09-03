559af72689804f6f380bf70811edeaa8663947f4
Ekamjot Singh <68366451+iekamjotsingh@users.noreply.github.com>
2020-09-03 12:43:25 +0530

    Updates

---
+++ precache-manifest.894282f957815ea005e42d58dfdf094d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b566c066ab2571e98eb1bd56e29439",
+    "url": "/Cryptosearch/index.html"
+  },
+  {
+    "revision": "ef7035aba38c477b91da",
+    "url": "/Cryptosearch/static/css/main.783f1279.chunk.css"
+  },
+  {
+    "revision": "0985992591117b3afc0a",
+    "url": "/Cryptosearch/static/js/2.78724af5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cryptosearch/static/js/2.78724af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7035aba38c477b91da",
+    "url": "/Cryptosearch/static/js/main.6782beb2.chunk.js"
+  },
+  {
+    "revision": "acf44256f127c3e286a2",
+    "url": "/Cryptosearch/static/js/runtime-main.a09c7cca.js"
+  }
+]);
